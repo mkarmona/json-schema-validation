3161ac5b69886ba75e1760d39d96341e91638b39
Gautier Koscielny <gautier.x.koscielny@gsk.com>
2015-01-06 12:09:49 +0000

    First tests for evidence string package

---
+++ src/python/tests/test_org_cttv_input_model.py
@@ -0,0 +1,22 @@
+import sys
+# the following dir contains the init file
+sys.path.append('../../../build')
+
+import copy
+import pickle
+
+import pytest
+
+import org.cttv.input.model as cttv
+
+def test_evidenceString_exists():
+    evidenceString = cttv.EvidenceString()
+    assert not evidenceString == None
+
+#def test_evidenceString_dummy():
+#    evidenceString = cttv.EvidenceString()
+#    assert 1 == 2
+
+def foo():
+  pass
+    
