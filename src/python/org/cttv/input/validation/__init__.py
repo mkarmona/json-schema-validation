e002cba89d0cb72f9f8ebd048856511ebcbd0204
Gautier Koscielny <gautier.x.koscielny@gsk.com>
2015-02-03 16:14:58 +0000

    package init file

---
+++ src/python/org/cttv/input/validation/__init__.py
@@ -0,0 +1,8 @@
+"""
+A sub-package for generating the cttv package and for validating evidence strings
+Additional (module-specific) information can be found in the docstring for the module of interest.
+"""
+from __future__ import absolute_import, print_function
+from .package_generator import PackageGenerator
+from .package_validator import PackageValidator
+
