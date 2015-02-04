37e7389c6448d5a9b4b841a6abcf92980f9e29b8
Gautier Koscielny <gautier.x.koscielny@gsk.com>
2015-02-04 17:55:28 +0000

    use nose as testing environment

--- src/python/org/cttv/input/validation/tests/test_org_cttv_input_model.py
+++ src/python/org/cttv/input/validation/tests/test_org_cttv_input_model.py
@@ -1,22 +1,21 @@
-import sys
-# the following dir contains the init file
-sys.path.append('.')
+from __future__ import absolute_import, print_function
 
-import copy
-import pickle
+import org.cttv.input.model as model
 
-import pytest
-
-import org.cttv.input.model as cttv
-
+def setup_module(module):
+    print ("") # this is to get a newline after the dots
+    print ("setup_module before anything in this file")
+ 
+def teardown_module(module):
+    print ("teardown_module after everything in this file")
+ 
+def my_setup_function():
+    print ("my_setup_function")
+ 
+def my_teardown_function():
+    print ("my_teardown_function")
+ 
+@with_setup(my_setup_function, my_teardown_function)
 def test_evidenceString_exists():
     evidenceString = cttv.EvidenceString()
     assert not evidenceString == None
-
-#def test_evidenceString_dummy():
-#    evidenceString = cttv.EvidenceString()
-#    assert 1 == 2
-
-def foo():
-  pass
-    
