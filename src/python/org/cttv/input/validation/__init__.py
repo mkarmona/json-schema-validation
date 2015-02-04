bf787d680dc28337ea3794ddbcdd8fa404b72cc3
Gautier Koscielny <gautier.x.koscielny@gsk.com>
2015-02-04 10:53:51 +0000

    DataModel package

--- src/python/org/cttv/input/validation/__init__.py
+++ src/python/org/cttv/input/validation/__init__.py
@@ -3,6 +3,6 @@ A sub-package for generating the cttv package and for validating evidence string
 Additional (module-specific) information can be found in the docstring for the module of interest.
 """
 from __future__ import absolute_import, print_function
-from .modelgenerator import PackageGenerator
-from .modelvalidator import PackageValidator
+from .modelgenerator import DataModelGenerator
+from .modelvalidator import DataModelValidator
 
